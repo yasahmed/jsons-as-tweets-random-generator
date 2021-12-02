80e31c7560919354d9a53851c4a7bfc2fbac1d92
Ahmed Ahmed <jnah.ahmed@gmail.com>
2021-12-03 00:57:17 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,28 @@
+var faker = require("faker");
+const express = require("express");
+
+const app = express();
+
+const generateTeweets = (count) => {
+  let tweets = [];
+  for (let i = 0; i < count; i++) tweets.push(generateTeweet());
+  return tweets;
+};
+
+const generateTeweet = () => {
+  return {
+    id: faker.datatype.uuid(),
+    name: faker.name.findName(),
+    creation_date: faker.date.soon(),
+    email: faker.internet.email(),
+    card: faker.helpers.createCard().posts[0],
+  };
+};
+
+app.get("/tweets", (req, res) => {
+  res.send(generateTeweets(10));
+});
+
+app.listen(8089, () => {
+  console.log("Serveur à l'écoute");
+});
